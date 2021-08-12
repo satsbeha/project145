efce132b53376bc850d0eb413341843211b8f5fa
sol68 <solmulesoft46@gmail.com>
2021-08-12 15:12:30 -0500

    cs415 project

--- app.js
+++ app.js
@@ -1,37 +1,4 @@
 
-/*
-const express = require('express');
-const cors = require('cors');
-const mongoConnect = require('./utils/database').mongoConnect;
-const bookRouter = require('./routes/bookRouter');
-//const authRouter = require('./routes/auth');
-
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-//app.use(authRouter); //all urls access after authRouter needs JWT
-app.use('/book', bookRouter);
-
-app.use((req, res, next) => {
-    res.status(404).json({ error: req.url + ' API not supported!' });
-});
-
-app.use((err, req, res, next) => {
-    if (err.message === 'NOT Found') {
-        res.status(404).json({ error: err.message });
-    } else {
-        res.status(500).json({ error: 'Something is wrong! Try later' });
-    }
-});
-
-mongoConnect(() => {
-    app.listen(3222, () => console.log('listening to 3222...'));
-});
-*/
-
-
 const express = require('express');
 const cors = require('cors');
 const mongoConnect = require('./utils/database').mongoConnect;
